ba314f8fb85ac60cee3b1df52aec1a5b47dce178
Michael Dinh <tonkoalsce@gmail.com>
2023-11-05 22:58:02 +1100

    Adding INotifyPropertyChanged for viewmodels and notes

--- States/Navigators/Navigator.cs
+++ States/Navigators/Navigator.cs
@@ -2,6 +2,7 @@
 using AlgoTrader.App.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,15 +16,43 @@ namespace AlgoTrader.App.States.Navigators
         Portfolio
     }
 
-    public class Navigator : INavigator
+    /*
+     * Small notes on INotifyPropertChanged
+     * How the INotifyProperyChanged works
+        When the view uses this as the view model and has a binding
+        In the background that view will know that the binding implements the PropertyChangedEventHandler
+        It will auto subscribe to the PropertyChangedEventHandler event.
+        Now when we have a proprty change we can update that event by calling PropertyChangedEventHandler usually done by implementing a 
+        helper method called OnPropertyChanged
+     */
+    public class Navigator : INavigator, INotifyPropertyChanged
     {
-        public ViewModelBase CurrentViewModel { get; set; } 
+        private ViewModelBase _viewModelBase;
+        public ViewModelBase CurrentViewModel
+        {
+            get
+            {
+                return _viewModelBase;
+            }
+            set
+            {
+                _viewModelBase = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
 
         public Navigator() 
         {
-            CurrentViewModel = new HomeViewModel();
+            _viewModelBase = new HomeViewModel();
         }
 
         public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
